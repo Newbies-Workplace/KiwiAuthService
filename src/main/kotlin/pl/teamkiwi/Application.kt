4e39a445147eda0820a28020534f6465b56ec8b6
Wiktor Petryszyn <jutuper@gmail.com>
2020-07-10 21:29:20 +0200

    fix

--- src/main/kotlin/pl/teamkiwi/Application.kt
+++ src/main/kotlin/pl/teamkiwi/Application.kt
@@ -106,7 +106,7 @@ fun Application.mainModule() {
 
     install(ConsulFeature) {
         consulUrl = getProp("kiwi.consul.url")
-        host = "http://127.0.0.1"
+        //host = "http://127.0.0.1"
 
         registrationConfig {
             check(Registration.RegCheck.http("$host:$port/health", 20))
